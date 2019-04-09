53b23c2a462130db11cf64a46814c085de1528fc
qs.zhou <qs.zhou@Ctrip.com>
2019-04-09 20:28:10 +0800

    Integration with spring

--- src/main/java/org/rhine/mybatis/demo/annotation/UserMapper.java
+++ src/main/java/org/rhine/mybatis/demo/annotation/UserMapper.java
@@ -1,14 +1,21 @@
 package org.rhine.mybatis.demo.annotation;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultType;
 import org.apache.ibatis.annotations.Select;
 import org.rhine.mybatis.demo.User;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface UserMapper {
 
     @ResultType(org.rhine.mybatis.demo.User.class)
     @Select("select * from user where id = #{id}")
     User queryUser(@Param("id") Long id);
+
+    @ResultType(Long.class)
+    @Delete("delete from user where id  = #{id}")
+    Long deleteUserById(@Param("id") Long id);
 
 }
--- src/test/java/org/rhine/mybatis/demo/spring/IntegrationWithSpring.java
+++ src/test/java/org/rhine/mybatis/demo/spring/IntegrationWithSpring.java
@@ -3,25 +3,22 @@ package org.rhine.mybatis.demo.spring;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.rhine.mybatis.demo.User;
 import org.rhine.mybatis.demo.annotation.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath*:spring-config.xml")
 public class IntegrationWithSpring {
 
     @Autowired
-    private SqlSessionTemplate sqlSessionTemplate;
+    private UserMapper userMapper;
 
     @Test
-    public void testQueryUser() {
-        UserMapper userMapper = sqlSessionTemplate.getMapper(UserMapper.class);
-        User user = userMapper.queryUser(1L);
-        Assert.assertNotNull(user);
+    @Transactional
+    public void testDeleteUserById() {
+        Assert.assertNotNull(userMapper.deleteUserById(1L));
     }
-
 }
